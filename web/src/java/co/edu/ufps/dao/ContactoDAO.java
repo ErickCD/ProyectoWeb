9e45c6bce1d684061c0c67db23db2c94f1446812
Clair <acheron_1405@hotmail.com>
2016-12-16 15:25:13 -0600

    cambios antes de entrega 8

--- web/src/java/co/edu/ufps/dao/ContactoDAO.java
+++ web/src/java/co/edu/ufps/dao/ContactoDAO.java
@@ -14,10 +14,10 @@ import java.util.logging.Logger;
  * @author Clair
  */
 public class ContactoDAO {
-    
+
     private Statement s = null;
     private conexion con = null;
-    
+
     public ContactoDAO() {
         try {
             con = new conexion();
@@ -33,7 +33,7 @@ public class ContactoDAO {
         boolean rs = false;
         Date date = new Date();
         String fecha = date.getYear() + 1900 + "-" + (date.getMonth() + 1) + "-" + date.toGMTString().substring(0, 1);
-        
+
         try {
             rs = s.execute("INSERT INTO `contacto`(`consecutivo`, `fecha`, `asesor`, `cto_nombres`, "
                     + "`cto_apellidos`, `cto_cc`, `cto_cargo`, `cto_cde`, `cto_direccion`, `cto_ciudad`, `cto_pais`, "
@@ -48,13 +48,13 @@ public class ContactoDAO {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage());
         }
-        
+
         return rs;
     }
-    
+
     public boolean updateContacto(Contacto cont) {
         boolean respuesta = false;
-        
+
         try {
             respuesta = s.execute("UPDATE `contacto` SET `cto_antiguedad_cargo`=\'" + cont.getCto_antiguedad_cargo() + "\',"
                     + "`cto_lugar_nacimiento`=\'" + cont.getCto_lugar_nacimiento() + "\',"
@@ -63,7 +63,7 @@ public class ContactoDAO {
                     + "`cto_etnia`=\'" + cont.getCto_etnia() + "\',`cto_condicion_desplazado`=\'" + cont.getCto_condicion_desplazado()
                     + "\', `cto_notas` = \'" + cont.getCto_notas() + "\' WHERE `cto_email` = \'" + cont.getCto_email() + "\';");
             return true;
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(ContactoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -73,7 +73,7 @@ public class ContactoDAO {
     //
     public List<Contacto> mostrarContacto(int cantidad) {
         int limite = cantidad + 10;
-        
+
         List<Contacto> list = new ArrayList<>();
         ResultSet rs;
         try {
@@ -104,7 +104,7 @@ public class ContactoDAO {
         } catch (SQLException ex) {
             Logger.getLogger(ContactoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return list;
     }
 
@@ -113,9 +113,8 @@ public class ContactoDAO {
         List<Contacto> list = new ArrayList<>();
         ResultSet rs;
         try {
-            rs = s.executeQuery("SELECT `cto_nombres`,`cto_apellidos`,`cto_cc`,`cto_cargo`,`cto_direccion`,`cto_ciudad`, `cto_pais`,"
-                    + " `cto_cecular`, `cto_fijo`, `cto_email`, `cto_email_masivo`, `cto_genero`,`cto_departamento` FROM `contacto`;");
-            
+            rs = s.executeQuery("SELECT * FROM `contacto`;");
+
             while (rs.next()) {
                 Contacto cont = new Contacto(
                         rs.getString("cto_nombres"),
@@ -131,22 +130,22 @@ public class ContactoDAO {
                         rs.getString("cto_email_masivo"),
                         rs.getString("cto_genero"),
                         rs.getString("cto_departamento"));
-                
+
                 list.add(cont);
             }
         } catch (SQLException ex) {
             Logger.getLogger(ContactoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return list;
     }
-    
+
     public List<Contacto> mostrarFiltroContactos(int cantidad) {
         ArrayList lista = new ArrayList();
         try {
             int limite = cantidad + 10;
             ResultSet rs = s.executeQuery("SELECT * FROM `contacto`order by cto_nombres asc limit " + cantidad + "," + limite + ";");
-            
+
             Contacto contacto = null;
             while (rs.next()) {
                 contacto = new Contacto(rs.getString("consecutivo"), rs.getString("fecha"), rs.getString("asesor"), rs.getString("cto_nombres"),
@@ -158,20 +157,20 @@ public class ContactoDAO {
             }
         } catch (Exception e) {
         }
-        
+
         return lista;
     }
-    
+
     public boolean borrarContacto(String cto_cc) {
         boolean respuesta = false;
-        
+
         try {
             respuesta = s.execute("DELETE FROM `contacto` WHERE `cto_cc` = \"" + cto_cc + "\";");
             return true;
         } catch (SQLException ex) {
             Logger.getLogger(ContactoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return respuesta;
     }
 
@@ -186,7 +185,7 @@ public class ContactoDAO {
             } else {
                 rs = s.executeQuery("SELECT * FROM `contacto` WHERE `cto_email` like \"" + valorBuscado + "%\" order by `cto_email`;");
             }
-            
+
             rs.next();
             aux = new Contacto(rs.getString("consecutivo"), rs.getString("fecha"), rs.getString("asesor"), rs.getString("cto_nombres"),
                     rs.getString("cto_apellidos"), rs.getString("cto_cc"), rs.getString("cto_cargo"), rs.getString("cto_direccion"),
@@ -197,7 +196,7 @@ public class ContactoDAO {
         } catch (SQLException ex) {
             Logger.getLogger(ContactoDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return null;
     }
     //Fin de busquedas.
@@ -208,9 +207,9 @@ public class ContactoDAO {
             rs.next();
             return rs.getInt("cto_id");
         } catch (SQLException e) {
-            
+
         }
         return 0;
     }
-    
+
 }
